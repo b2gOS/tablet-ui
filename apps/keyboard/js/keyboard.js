e0b734a66dbfc25b6757da185f829b5e7f996435
ittat <ittat@live.com>
2021-05-02 08:36:06 +0800

    [WEBAPPS] Modify the layout of the keyboard and System. Adjust top-site

--- apps/keyboard/js/keyboard.js
+++ apps/keyboard/js/keyboard.js
@@ -10,6 +10,15 @@ function addLine(container, line) {
         container.appendChild(row);
 }
 
+function addFunctionLine(container){
+  var FunctionLine = '<div id="cvc">' +
+              '<span style="width: 3em;">Enter</span>' +
+              '<span style="width: 7em;">Space</span>' +
+              '<span style="width: 3em;">Del</span>' +
+              '</div>';
+        container.insertAdjacentHTML('beforeend', FunctionLine);
+}
+
 document.addEventListener(
         "DOMContentLoaded",
         () => {
@@ -18,22 +27,36 @@ document.addEventListener(
           addLine(vkb, "qwertyuiop");
           addLine(vkb, "asdfghjkl;");
           addLine(vkb, "zxcvbnm,./");
+          addFunctionLine(vkb);
 
           vkb.addEventListener("click", event => {
             let key = event.target.textContent;
             console.log(`Event 'click' on ${key}`);
-            let ime = navigator.b2g.inputMethod;
-            ime
-              .setComposition(key)
-              .then(ime.endComposition(key))
-              .then(
-                () => {
-                  console.log(`IME adding ${key} ok`);
-                },
-                () => {
-                  console.log(`IME error adding ${key}`);
-                }
-              );
+            switch (key) {
+              case 'Enter':
+                navigator.b2g.inputMethod.sendKey("Enter");
+                break;
+              case 'Space':
+                navigator.b2g.inputMethod.sendKey(" ");
+                break;
+              case 'Del':
+                navigator.b2g.inputMethod.deleteBackward();
+                break;
+              default:
+                let ime = navigator.b2g.inputMethod;
+                ime
+                  .setComposition(key)
+                  .then(ime.endComposition(key))
+                  .then(
+                    () => {
+                      console.log(`IME adding ${key} ok`);
+                    },
+                    () => {
+                      console.log(`IME error adding ${key}`);
+                    }
+                  );    
+                break;
+            }
           });
 
         },
